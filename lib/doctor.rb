e831ff6d4392eb0994c313a1e5aab9e7f39125a9
Mohammed Noori <kmanoorib@gmail.com>
2018-06-25 22:36:40 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,18 @@
+class Doctor
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(patient, self, date)
+  end
+  
+  def self.all
+    @@all
+  end
+end
